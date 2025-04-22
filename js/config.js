ba75cb9db981fc4fbfa59135362ea31a6ad72619
Bajer123 <frejahartvig@hotmail.com>
2025-04-22 13:19:38 +0200

    Added code for google sheets

---
+++ js/config.js
@@ -0,0 +1,14 @@
+// Configuration settings for the experiment
+
+// Replace with your deployed Apps Script URL
+const scriptURL = "https://script.google.com/macros/s/AKfycbxey8k8rpQxOtrV-TN-CknQP5L8Oj6u-JrcFN3DDMLrfbWvEeGvlnsqy1NebIXeGBm4_A/exec";
+
+// Image display duration in milliseconds
+const imageDisplayDuration = 3000;
+
+// Image data structure with ids and paths
+const imageData = [
+  { id: "lux", path: "images/lux.jpg" },
+  { id: "valo", path: "images/valo.jpg" },
+  { id: "rando", path: "images/league-of-legends-mobas-diversity-dota-2.jpg" }
+];
